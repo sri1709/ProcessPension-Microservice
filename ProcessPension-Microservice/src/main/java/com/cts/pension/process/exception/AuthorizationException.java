e59b40f2ca4bb0da931416f81f691574a706934e
sri1709 <58382463+sri1709@users.noreply.github.com>
2022-05-26 21:51:36 +0530

    Delete ProcessPension-Microservice directory

--- ProcessPension-Microservice/src/main/java/com/cts/pension/process/exception/AuthorizationException.java
+++
@@ -1,9 +0,0 @@
-package com.cts.pension.process.exception;
-
-@SuppressWarnings("serial")
-public class AuthorizationException extends Exception{
-
-	public AuthorizationException(String message) {
-		super(message);
-	}
-}
