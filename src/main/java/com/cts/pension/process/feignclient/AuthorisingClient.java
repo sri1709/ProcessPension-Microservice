6cc39eb7f28148d47bdafe4ecafa16b558c8fc09
sri1709 <58382463+sri1709@users.noreply.github.com>
2022-05-29 23:37:28 +0530

    Update AuthorisingClient.java

--- src/main/java/com/cts/pension/process/feignclient/AuthorisingClient.java
+++ src/main/java/com/cts/pension/process/feignclient/AuthorisingClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "Authorizatiion-Microservice", url = "http://localhost:8400/auth/api/v1")
+@FeignClient(name = "Authorizatiion-Microservice", url = "http://PMS-877629-Microservices-lb-1165068758.us-east-1.elb.amazonaws.com/auth/api/v1")
 public interface AuthorisingClient {
 
 	@PostMapping("/authorize")
